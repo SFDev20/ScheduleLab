1ba285cd959fa6f26c678263ea66fbee5f9bc01b
SFDev <sfdev@decodevs.net>
2025-09-20 03:09:53 -0500

    style: set grid layout for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import "./page.css"
+
 export default function Home() {
   return (
-    <>
-    </>
+    <div id="Mix-Panel">
+      <div id="Drugs-List">
+      </div>
+      <div id="Additives-List">
+      </div>
+      <div id="Interactive-Mix">
+      </div>
+      <div id="Historial-List">
+      </div>
+    </div>
   );
 }
