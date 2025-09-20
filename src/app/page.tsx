24dcd2ccc046b0f11810b9b18582664bd12beec3
SFDev <sfdev@decodevs.net>
2025-09-20 04:28:07 -0500

    style: set design for additives list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,22 @@ export default function Home() {
         <button><Image src={"/images/drugs/Cocaine.webp"} width={75} height={75} alt="cocaine"/></button>
       </div>
       <div id="Additives-List">
+        <button><Image src={"/images/additives/Addy.webp"} width={70} height={70} alt="ogkush"/></button>
+        <button><Image src={"/images/additives/Banana.webp"} width={70} height={70} alt="sourdiesel"/></button>
+        <button><Image src={"/images/additives/Battery.webp"} width={70} height={70} alt="greencrack"/></button>
+        <button><Image src={"/images/additives/Chili.webp"} width={70} height={70} alt="granddaddypurple"/></button>
+        <button><Image src={"/images/additives/Cuke.webp"} width={70} height={70} alt="meth"/></button>
+        <button><Image src={"/images/additives/Donut.webp"} width={70} height={70} alt="cocaine"/></button>
+        <button><Image src={"/images/additives/Energy_Drink.webp"} width={70} height={70} alt="ogkush"/></button>
+        <button><Image src={"/images/additives/Flu_Medicine.webp"} width={70} height={70} alt="sourdiesel"/></button>
+        <button><Image src={"/images/additives/Gasoline.webp"} width={70} height={70} alt="greencrack"/></button>
+        <button><Image src={"/images/additives/HorseSemen.webp"} width={70} height={70} alt="granddaddypurple"/></button>
+        <button><Image src={"/images/additives/Iodine.webp"} width={70} height={70} alt="meth"/></button>
+        <button><Image src={"/images/additives/Mega_Bean.webp"} width={70} height={70} alt="cocaine"/></button>
+        <button><Image src={"/images/additives/Motor_Oil.webp"} width={70} height={70} alt="greencrack"/></button>
+        <button><Image src={"/images/additives/Mouth_Wash.webp"} width={70} height={70} alt="granddaddypurple"/></button>
+        <button><Image src={"/images/additives/Paracetamol.webp"} width={70} height={70} alt="meth"/></button>
+        <button><Image src={"/images/additives/Viagra.webp"} width={70} height={70} alt="cocaine"/></button>
       </div>
       <div id="Interactive-Mix">
       </div>
