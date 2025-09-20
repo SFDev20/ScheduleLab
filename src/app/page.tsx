4b0454c7c365348afbcd5e9aeb006e6add26d907
SFDev <sfdev@decodevs.net>
2025-09-20 04:03:57 -0500

    style: set design for drugs list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import "./page.css"
 
 export default function Home() {
   return (
     <div id="Mix-Panel">
       <div id="Drugs-List">
+        <button><Image src={"/images/drugs/OGKush.webp"} width={75} height={75} alt="ogkush"/></button>
+        <button><Image src={"/images/drugs/SourDiesel.webp"} width={75} height={75} alt="sourdiesel"/></button>
+        <button><Image src={"/images/drugs/GreenCrack.webp"} width={75} height={75} alt="greencrack"/></button>
+        <button><Image src={"/images/drugs/GranddaddyPurple.webp"} width={75} height={75} alt="granddaddypurple"/></button>
+        <button><Image src={"/images/drugs/Meth.webp"} width={75} height={75} alt="meth"/></button>
+        <button><Image src={"/images/drugs/Cocaine.webp"} width={75} height={75} alt="cocaine"/></button>
       </div>
       <div id="Additives-List">
       </div>
